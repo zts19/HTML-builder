dc149556f4bfaf7477048f82bc350589cb8716ab
zts19 <zankovets19@mail.ru>
2022-11-07 19:46:26 +0300

    feat: completed 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,20 @@
+const fs = require("fs/promises");
+const path = require("path");
 
+
+const copyDir = async (fromPath, toPath) => {
+    await fs.rm(path.resolve(__dirname, 'files-copy'), { recursive: true, force: true });
+    await fs.mkdir(path.resolve(__dirname, 'files-copy'), { recursive: true });
+    try {
+        let files = await fs.readdir(path.resolve(__dirname, 'files'))
+        files.forEach( e => {
+            const from = path.resolve(__dirname, 'files', e)
+            const to = path.resolve(__dirname, 'files-copy', e)
+            fs.copyFile(from, to)
+        })
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+copyDir()
